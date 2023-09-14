adecb17dc428da5d46269e7fe78eda44db79758d
Jannick Oste <jannick@oste.dev>
2023-09-14 15:23:57 +0200

    Removed need for DbSet, entities now added using attribute

--- Api/App/Core/Domain/Database/DbEntityAttribute.cs
+++ Api/App/Core/Domain/Database/DbEntityAttribute.cs
@@ -1,3 +1,14 @@
+using System.Collections.Immutable;
+
 namespace App.Core.Domain.Database;
 
-public class DbEntityAttribute : Attribute {}
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class DbEntityAttribute : Attribute {
+    private static List<Type> targetModels = new List<Type>();
+    public static ImmutableList<Type> TargetModels { get => targetModels.ToImmutableList();}
+
+    public DbEntityAttribute(Type type)
+    {
+        targetModels.Add(type);
+    }
+}
