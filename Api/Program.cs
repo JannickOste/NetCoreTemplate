33bf1d2c5b30ca90c9eeff4827e9278474c44d0f
Jannick Oste <jannick@oste.dev>
2023-09-15 17:20:26 +0200

    Sysadmin added for docker setup/start

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,20 +1,11 @@
 using System.Net;
 
 
-/**
-* @author: Oste Jannick
-* @description: The main entry point of the .net core project were all server configuration takes place. 
-* @Date: 2023-09-11
-*/
 await Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
     config => config
     .UseStartup<App.Core.Infrastructure.Startup.Startup>()
-        .UseUrls("https://localhost:6001") // Change the port if needed
         .UseKestrel(options =>
         {
-            options.Listen(IPAddress.Loopback, 6001, listenOptions =>
-            {
-                listenOptions.UseHttps(); // HTTPS
-            });
+            options.Listen(IPAddress.Loopback, 80);
         })
 ).Build().RunAsync();
