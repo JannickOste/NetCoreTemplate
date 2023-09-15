86619dacd17b4678cde516e014a2737b7f3c36e2
Jannick Oste <jannick@oste.dev>
2023-09-15 17:39:17 +0200

    HTTP support for docker

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,8 +4,4 @@ using System.Net;
 await Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
     config => config
     .UseStartup<App.Core.Infrastructure.Startup.Startup>()
-        .UseKestrel(options =>
-        {
-            options.Listen(IPAddress.Loopback, 80);
-        })
 ).Build().RunAsync();
